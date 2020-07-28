b1d712cc8a89a5792f37c95185d308f276d49363
unknown <alansm9790@gmail.com>
2020-07-28 11:02:05 -0500

    Amazon

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 });
 
 
-app.set('port', process.env.PORT || 80)
+app.set('port', process.env.PORT || 443)
 
 /* ------------Router--------------------------------- */
 
